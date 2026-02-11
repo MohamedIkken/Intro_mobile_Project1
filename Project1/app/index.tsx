84b45fda0c9816d5e3087a84ccc146ae207fc79a
HaiSam29 <s127667@ap.be>
2026-02-11 16:56:59 +0100

    Add expo project

---
+++ Project1/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Dit is een test!!!!!!!!!hehehehehahahaheheh coooool</Text>
+    </View>
+  );
+}
+
+export default Index;
