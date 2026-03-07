65ced3ddd5ec5221af1aa86b129ca28972113f66
HaiSam29 <s127667@ap.be>
2026-03-07 16:11:37 +0100

    feat: update Firebase configuration with project-specific credentials and remove measurement ID

--- Project1/firebaseConfig.js
+++ Project1/firebaseConfig.js
@@ -26,7 +26,6 @@ const firebaseConfig = {
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
